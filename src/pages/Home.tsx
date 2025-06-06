47210b6c2a8a02ccd232a80d64c5ca5d9f5ea634
Sarah BOURGEOIS <sarah.bourgeois.bc@gmail.com>
2025-06-07 01:29:27 +0200

    feat(core): improve code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
-// src/pages/Home.tsx
 import HeroSection from "../components/features/home/HeroSection";
 import PracticalInformation from "../components/features/home/PracticalInformation";
 import SalonGallery from "../components/features/home/SalonGallery";
 import ServiceSection from "../components/features/home/ServiceSection";
 import TeamSection from "../components/features/home/TeamSection";
+
 
 export default function Home() {
   return (
