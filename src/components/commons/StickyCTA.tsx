b59c2b3e0d709b07d49ada283a73246a8067bf1e
Sarah BOURGEOIS <sarah.bourgeois.bc@gmail.com>
2025-06-07 02:52:44 +0200

    fix(responsive): fix sticky cta for mobile

--- src/components/commons/StickyCTA.tsx
+++ src/components/commons/StickyCTA.tsx
@@ -1,14 +1,14 @@
-export const StickyCTA = ({ href, text }: { href: string, text: string }) => {
+export const StickyCTA = ({ href, text }: { href: string; text: string }) => {
   return (
-      <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50">
-        <a
-          href={href}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block bg-emerald-700 hover:bg-emerald-600 text-white font-semibold px-6 py-3 rounded-full shadow-xl transition"
-        >
-          {text}
-        </a>
-      </div>
-  )
-}
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-50 w-full px-4 md:px-0">
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block text-center max-w-xs mx-auto bg-emerald-700 hover:bg-emerald-600 text-white font-semibold px-6 py-3 rounded-full shadow-xl transition whitespace-nowrap"
+      >
+        {text}
+      </a>
+    </div>
+  );
+};
